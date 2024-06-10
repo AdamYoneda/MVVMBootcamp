81080cc661215e7b958bf131147ab0bd4ed91995
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-10 17:57:54 +0900

    update: MainViewControllerのtableView関連のコードをextensionとしてまとめるファイルを追加

---
+++ MVVMBootcamp/Controllers/MainViewController/MainViewController+TableView.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController+TableView.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/10.
+//
+
+import Foundation
+import UIKit
+/***
+ MainViewControllerのtableView関連のコードをextensionとしてまとめる
+ ***/
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0 // 仮
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell() // 仮
+    }
+}

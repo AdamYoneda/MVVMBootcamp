ceec6e1e33b6157e0f13a21efa802929749623f4
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-12 20:30:26 +0900

    update: MainViewControllerに関連するViewModelの作成

---
+++ MVVMBootcamp/ViewModel/MainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewModel.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/12.
+//
+
+// MainViewControllerに関連するViewModel
+
+import Foundation
+
+class MainViewModel {
+    
+}

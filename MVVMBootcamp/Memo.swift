2d9441ed565bf4aa23d26b48cb4427ebfc7a5d30
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-12 20:25:38 +0900

    add: memoファイルを追加

---
+++ MVVMBootcamp/Memo.swift
@@ -0,0 +1,22 @@
+//
+//  Memo.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/12.
+//
+
+import Foundation
+
+/*
+ 
+ Chapter 2
+ 8:30~
+ All the logics related to the each view controller or each view should be happen inside the view model.
+ A view model  shouldn't have all the logics and the view controllers should be just as dummy as possible.
+ It just asks the view model what should I present and then the view model responsibility is that to response back to the view controller to the view to presenting data.
+ 各ビューコントローラや各ビューに関連するすべてのロジックは、ビューモデルの内部で発生するはずです。
+ ビューモデルはすべてのロジックを持つべきではありませんし、ビューコントローラーはできるだけダミーであるべきです。
+ ビューモデルは、ビューコントローラーやビューにデータを提示するために、ビューコントローラーやビューに返答する役割を担っています。
+ 
+ 
+ */

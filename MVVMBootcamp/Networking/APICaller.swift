dea57e9bc561cc8d4d3c0c4ecb40937d5c00cc2e
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-14 20:05:18 +0900

    feat: APICaller.swiftファイルの作成

---
+++ MVVMBootcamp/Networking/APICaller.swift
@@ -0,0 +1,17 @@
+//
+//  APICaller.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/14.
+//
+
+/*
+ 拡張性のあるnetworkingクラス
+ 
+ */
+
+import Foundation
+
+public class APICaller {
+    
+}

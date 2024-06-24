c3c04b51033e04aa591a4af05e40dd6b70906958
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-24 18:45:07 +0900

    add: 表示するセルのファイル、フォルダを追加

---
+++ MVVMBootcamp/View/MainMovieCell/MainMovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainMovieCell.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/24.
+//
+
+import UIKit
+
+class MainMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}

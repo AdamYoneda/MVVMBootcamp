a34cd43bf70b16369fc585a9fa73ed6027722655
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-28 16:27:20 +0900

    update: CellViewModelとCellの連結

--- MVVMBootcamp/View/MainMovieCell/MainMovieCell.swift
+++ MVVMBootcamp/View/MainMovieCell/MainMovieCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MainMovieCell: UITableViewCell {
 
@@ -25,6 +26,9 @@ class MainMovieCell: UITableViewCell {
     }
     
     func setupCell(viewModel: MovieTableCellViewModel) {
-        
+        nameLabel.text = viewModel.title
+        dateLabel.text = viewModel.date
+        rateLabel.text = viewModel.rating
+        movieImageView.sd_setImage(with: viewModel.imageUrl)
     }
 }

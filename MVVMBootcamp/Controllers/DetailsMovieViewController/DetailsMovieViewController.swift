f7c1e44e8becacf890ad789b31b898c9f107031d
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-30 17:58:33 +0900

    update: UIパーツの配置と接続

--- MVVMBootcamp/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
+++ MVVMBootcamp/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class DetailsMovieViewController: UIViewController {
 
+    // IBOutlets:
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         

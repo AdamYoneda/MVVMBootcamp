260f8b6e266e81112743443e77f82c5194ba5548
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-06-30 16:58:34 +0900

    add: DetailsMovieViewControllerファイルの追加

---
+++ MVVMBootcamp/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsMovieViewController.swift
+//  MVVMBootcamp
+//
+//  Created by Adam Yoneda on 2024/06/30.
+//
+
+import UIKit
+
+class DetailsMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}

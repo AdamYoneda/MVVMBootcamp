db506a57234055ef58e1e505b7b62d6a54256898
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-01 15:22:41 +0900

    update: 詳細画面で必要な情報が少ないため、viewModel内にプロパティを追加

--- MVVMBootcamp/ViewModel/DetailsMovieViewModel.swift
+++ MVVMBootcamp/ViewModel/DetailsMovieViewModel.swift
@@ -12,7 +12,22 @@ class DetailsMovieViewModel {
     
     var movie: Movie
     
+    var movieImage: URL?
+    var movieTitle: String
+    var movieDescription: String
+    var movieID: Int
+    
     init(movie: Movie) {
         self.movie = movie
+        
+        self.movieID = movie.id
+        self.movieTitle = movie.title ?? movie.name ?? "UNKNOWN"
+        self.movieDescription = movie.overview ?? ""
+        self.movieImage = makeImageURL(movie.backdropPath ?? "")
+    }
+    
+    private func makeImageURL(_ imageCode: String) -> URL? {
+        let urlString = NetworkConstant.shared.imageServerAdress + imageCode
+        return URL(string: urlString)
     }
 }
